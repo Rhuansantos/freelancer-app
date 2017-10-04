daf8c20eb358ca6af58e821642ebc21f01794bb1
Rhuan Souza <rhuansantosdev@gmail.com>
2017-10-04 14:33:47 -0400

    import redux and help function to creating help function to create reducers

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReduxers, compose } from 'redux';
+import thunkMiddleware from 'redux-logger';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app</Text>
+        <Text>Open up App.js to start working on your app!</Text>
       </View>
     );
   }
