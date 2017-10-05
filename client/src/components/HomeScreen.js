632a56313c3f31299fbf1b3a6e9b160448dc88c0
Rhuan Souza <rhuansantosdev@gmail.com>
2017-10-04 23:10:00 -0400

    gradient background

--- client/src/components/HomeScreen.js
+++ client/src/components/HomeScreen.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Button, StyleSheet, Text, View } from 'react-native';
+import AnimatedLinearGradient from 'react-native-animated-linear-gradient';
+
+import Search from './Search';
+
 
 class HomeScreen extends Component{
+
+	constructor(){
+		super();
+		this.presetColors = {
+			mainBackground: [
+				'rgb(54, 104, 151)',
+				'rgb(54, 120, 159)',
+				'rgb(55, 133, 166)',
+				'rgb(56, 145, 173)',
+				'rgb(57, 158, 180)',
+				'rgb(59, 171, 187)',
+				'rgb(60, 185, 184)'
+			],
+		};
+	}
+	
 	render() {
 		return (
-			<View>
-				<Text>Hi</Text>
-				    <Button
-					onPress={() => this.props.navigation.dispatch({ type: 'Profile' })}
-					title="Log in"
-					/>
-			</View>
+			<AnimatedLinearGradient customColors={this.presetColors.mainBackground} speed={4000}>
+				<Search />
+			</AnimatedLinearGradient>
 		);
 	}
 }
