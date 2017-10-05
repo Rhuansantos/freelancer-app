121cc46422f14df83fb9c9d27f8aec8148aae9e4
Rhuan Souza <rhuansantosdev@gmail.com>
2017-10-05 01:32:00 -0400

    include icon search

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Text, View, TextInput, Dimensions } from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
+import { Hideo } from 'react-native-textinput-effects';
 
 export default class Search extends Component {
 	constructor(props) {
@@ -11,20 +12,18 @@ export default class Search extends Component {
   
 	render() {
 	  return (
-		<View style={styles.container}>
-			<Icon name="ios-person" size={30} color="white" />
-		  <TextInput
-			style={styles.searchInput}
-			placeholderTextColor="#336B9C"
-			placeholder="What are you looking for?"
-			onChangeText={(text) => this.setState({text})}
-		  />
-		  <View style={styles.searchResultsContainer}>
-		  	<Text>
-			{this.state.text.split(' ').map((word) => word && 'results here').join(' ')}
-			</Text>
-		  </View>
-		</View>
+			<View style={styles.searchSection}>
+				<Hideo
+				iconClass={FontAwesomeIcon}
+				iconName={'search'}
+				iconColor={'#336B9C'}
+				placeholderTextColor="#336B9C"
+				placeholder="What are you looking for?"
+				// this is used as backgroundColor of icon container view.
+				iconBackgroundColor={'#E6E6EA'}
+				inputStyle={{ color: '#336B9C', backgroundColor: '#E6E6EA'}}
+				/>
+			</View>
 	  );
 	}
   }
@@ -32,22 +31,33 @@ export default class Search extends Component {
   const {height, width} = Dimensions.get('window');
 
   const styles = {
-	searchInput: {
+	searchSection: {
 		width: width / 1.1,
-		height: 50,
-		fontSize: 22,
-		color: '#336B9C',
+		// width: 300,
+		height: 60,
+		// paddingTop: 5,
+		padding: 5,
+		// flex: 1,
+		// flexDirection: 'row',
+		// justifyContent: 'center',
+		// alignItems: 'center',
 		backgroundColor: '#E6E6EA',
 		borderBottomColor: '#E6E6EA',
-		padding: 10,
 		borderRadius: 10
 	},
+	searchInput: {
+		// height: 50,
+		fontSize: 22,
+		// padding: 10,
+		color: '#336B9C',
+	},
+	searchIcon: {
+		// padding: 10,
+	},
 	searchResultsContainer: {
-		backgroundColor: '#E6E6EA',
-		width: width / 1.1,
-		height: 50,
-		// marginTop: -10,
-		borderRadius: 10
+		// flex: 1,
+		// backgroundColor: '#E6E6EA',
+		// borderRadius: 10
 	}
   };
   
