a8a203aac4f33062e1b65a0e3251fd6d1fe96d24
Rhuan Souza <rhuansantosdev@gmail.com>
2017-10-04 23:53:21 -0400

    change search Dimensions

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Button, StyleSheet, Text, View, TextInput, PixelRatio } from 'react-native';
+import { Button, StyleSheet, Text, View, TextInput, PixelRatio, Dimensions } from 'react-native';
 
 export default class Search extends Component {
 	constructor(props) {
@@ -17,22 +17,34 @@ export default class Search extends Component {
 			placeholder="What are you looking for?"
 			onChangeText={(text) => this.setState({text})}
 		  />
-		  <Text style={{padding: 10, fontSize: 42}}>
+		  <View style={styles.searchResultsContainer}>
+		  	<Text>
 			{this.state.text.split(' ').map((word) => word && 'results here').join(' ')}
-		  </Text>
+			</Text>
+		  </View>
 		</View>
 	  );
 	}
   }
 
+  const {height, width} = Dimensions.get('window');
+
   const styles = {
 	searchInput: {
+		width: width / 1.1,
 		height: 50,
 		fontSize: 22,
 		color: '#336B9C',
 		backgroundColor: '#E6E6EA',
 		borderBottomColor: '#E6E6EA',
 		padding: 10,
+		borderRadius: 10
+	},
+	searchResultsContainer: {
+		backgroundColor: '#E6E6EA',
+		width: width / 1.1,
+		height: 50,
+		// marginTop: -10,
 		borderRadius: 10
 	}
   };
