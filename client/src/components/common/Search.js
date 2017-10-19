0f4047bdeed4cd92c5492f4d4ee60f430a711db6
Rhuan Souza <rhuansantosdev@gmail.com>
2017-10-19 17:57:02 -0400

    more load screens and couple changes on search style

--- client/src/components/common/Search.js
+++ client/src/components/common/Search.js
@@ -1,12 +1,13 @@
 import  React, { Component }  from 'react';
 import {
-  AppRegistry, StyleSheet, Text, View, AlertIOS, Dimensions, PixelRatio
+  AppRegistry, StyleSheet, Text, View, AlertIOS, Dimensions, PixelRatio, Button
 } from 'react-native';
 // import axios from 'axios';
 
 import AutoComplete from 'react-native-autocomplete';
 import Services from '../../data.json';
 
+const {height, width} = Dimensions.get('window');
 
 class Search extends Component {
   constructor(props) {
@@ -23,78 +24,77 @@ class Search extends Component {
 	}
 
   onTyping(text) {
-    // const temp = [];
     const Services = this.state.Search
         .filter(services => services.name.toLowerCase().startsWith(text.toLowerCase()))
         .map(services => services.name);
 
     let temp = Array.from(new Set(Services)); // eliminate data that has been repetead
     this.setState({ data: temp });
-    
-    // console.log(this.state.data);
-
   }
   
   onSelect(value) {
-    AlertIOS.alert('You choosed', value);
+    // AlertIOS.alert('You choosed', value);
+    // console.log(this.props);
   }
 
   render() {
     return (
-      <View style={styles.container}>
-      {console.log(this.state.data)}
-        <AutoComplete
-          style={styles.autocomplete}
+        <View style={styles.container}>
+          <AutoComplete
+            style={styles.autocomplete}
 
-          suggestions={this.state.data}
-          onTyping={this.onTyping}
-          onSelect={this.onSelect}
+            suggestions={this.state.data}
+            onTyping={this.onTyping}
+            onSelect={this.onSelect}
 
-          placeholder="What are you looking for?"
-          clearButtonMode="always"
-          returnKeyType="go"
-          textAlign="center"
-          clearTextOnFocus
+            placeholder="What are you looking for?"
+            clearButtonMode="always"
+            returnKeyType="go"
+            textAlign="center"
+            clearTextOnFocus
 
-          autoCompleteTableTopOffset={10}
-          autoCompleteTableLeftOffset={20}
-          autoCompleteTableSizeOffset={-40}
-          autoCompleteTableBorderColor="transparent"
-          autoCompleteTableBackgroundColor="transparent"
-          autoCompleteTableCornerRadius={8}
-          autoCompleteTableBorderWidth={1}
+            autoCompleteTableTopOffset={10}
+            autoCompleteTableLeftOffset={20}
+            autoCompleteTableSizeOffset={-40}
+            autoCompleteTableBorderColor="transparent"
+            autoCompleteTableBackgroundColor="transparent"
+            autoCompleteTableCornerRadius={8}
+            autoCompleteTableBorderWidth={1}
 
-          autoCompleteFontSize={15}
-          autoCompleteRegularFontName="Helvetica Neue"
-          autoCompleteBoldFontName="Helvetica Bold"
-          autoCompleteTableCellTextColor={'white'}
+            autoCompleteFontSize={18}
+            autoCompleteRegularFontName="Helvetica Neue"
+            autoCompleteBoldFontName="Helvetica Bold"
+            autoCompleteTableCellTextColor={'white'}
 
-          autoCompleteRowHeight={40}
-          autoCompleteFetchRequestDelay={100}
+            autoCompleteRowHeight={40}
+            autoCompleteFetchRequestDelay={100}
 
-          maximumNumberOfAutoCompleteRows={10}
-        />
+            maximumNumberOfAutoCompleteRows={10}
+          />
+          <View style={styles.searchLine}/>
       </View>
     );
   }
 }
-
-
-const {height, width} = Dimensions.get('window');
 
 const styles = StyleSheet.create({
   autocomplete: {
 	  width: width / 1.1,
     height: 50,
     margin: 10,
-    marginTop: 50,
+    marginTop: 100,
     color: 'white',
-    borderBottomColor: 'white',
-		paddingBottom: 10,
-		borderBottomWidth: 1 / PixelRatio.get()
+    paddingBottom: 10,
+    fontSize: 28
+  },
+  searchLine: {
+    width: width / 1.2,
+    height: 1 / PixelRatio.get(),
+    backgroundColor: 'white',
   },
  container: {
     flex: 1,
+    alignItems:'center'
   }
 });
 
